feea2faa6320e50a2786a90189c81a1897343256
vadym <vadymlozovoi@gmail.com>
2022-07-10 00:51:47 +0300

    added variable counting the sum of calls functions, witch send any request to API. Added function changing API key in axios when this variable is equal to available max requests

--- axios.js
+++ axios.js
@@ -1,10 +1,35 @@
-//дописать некий механизм свитча, который будет заменять baseURL, когда будет достигнут лимит запросов
-//на текущий день
-
 const axios = require('axios')
+let {count} = require('./requests')
+const API_KEYS = ['BF09A3E0-DD14-4D51-B199-AF54015CF579',
+    '834C1385-CF84-4716-98BA-10A7301A552F',
+    '13F314D3-41C2-4549-ACC1-82E039EF323D']
+const apiKey = {
+    currentIndex: 0,
+    value: API_KEYS[this.currentIndex],
+    next: function (){
+       this.value = API_KEYS[this.currentIndex++]
+    },
+    restart: function (){
+       this.currentIndex = 0;
+    }
+}
+//функция, которая меняет ключ Api на другой в массиве, если функции с запросами на сервер были вызваны более 100 раз
+function changeApiKey(maxRequests = 100) {
+    if(count === maxRequests && apiKey.currentIndex <= API_KEYS.length) {
+        apiKey.next()
+        return count = 0
+    }
+    if(count === maxRequests && apiKey.currentIndex > API_KEYS.length) {
+        apiKey.restart()
+        return count = 0;
+    }
+}
+//установка интервала проверки
+setInterval(changeApiKey, 60000);
 
 module.exports = axios.create({
     baseURL: 'https://rest.coinapi.io/v1',
-    headers: {'X-CoinAPI-Key': '834C1385-CF84-4716-98BA-10A7301A552F'}
+    headers: {'X-CoinAPI-Key': apiKey.value},
 })
+
 
--- requests.js
+++ requests.js
@@ -3,11 +3,12 @@ const axios = require('./axios')
 const express = require('express');
 const CURRENCIES = require("./currencies");
 const app = express(); //поместить на Heroku
-
+let count = 0;
 
 const getCurrency = async (text) => {
     try {
         const result = await axios.get(`/exchangerate/${text}/USD`)
+        count++;
         return result.data.rate;
     } catch (error) {
         console.log(error);
@@ -16,15 +17,14 @@ const getCurrency = async (text) => {
 
 const getAllCurrencies = async () => {
     try {
-
         const result = await axios.get(`/assets`);
-        return result.data.filter((item) => CURRENCIES.includes(item.asset_id))
+        count++;
+        return  result.data.filter((item) => CURRENCIES.includes(item.asset_id))
             .map((item) => `${item.asset_id}: ${item.price_usd.toFixed(4)} usd`)
             .join("\n");
-
     } catch (error) {
         console.log(error);
     }
 };
 
-module.exports = {getCurrency, getAllCurrencies}
+module.exports = {getCurrency, getAllCurrencies, count}
