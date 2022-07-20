96829dc8cec93ee0f4d649b36710e8f30b1ff745
vadym <vadymlozovoi@gmail.com>
2022-07-20 19:26:36 +0300

    small refactoring

--- src/helpers/user.info.js
+++ src/helpers/user.info.js
@@ -1,20 +1,15 @@
 const CURRENCIES = require('../currencies')
 
-//TODO: -исправить костыли
 const getUserId = (ctx) => {
-    try {
-        return +ctx.update.callback_query.from.id;
-    } catch (error) {
-        return +ctx.update.message.from.id;
-    }
+    return ctx.update?.callback_query
+        ? +ctx.update.callback_query.from.id
+        : +ctx.update.message.from.id
 }
 
 const getSymbol = (ctx) => {
-    try {
-        return CURRENCIES.map((item) => ctx.update.callback_query.message.text.match(item)).join("");
-    } catch (error) {
-        return ctx.update.message.text;
-    }
+    return ctx.update?.callback_query
+        ? CURRENCIES.map((item) => ctx.update.callback_query.message.text.match(item)).join("")
+        : ctx.update.message.text
 }
 
 module.exports = {getUserId, getSymbol}
